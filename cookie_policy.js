f7a97b3c70feb8d1837c3e0c36ef01ab5d80c585
Marco Segato <marco.segato@gmail.com>
2015-06-14 14:46:36 +0200

    Cookie Consent plugin by Silktide

---
+++ cookie_policy.js
@@ -0,0 +1,14 @@
+// Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent
+window.cookieconsent_options = {
+    // Text message to display inside the banner
+    "message":"Questo sito o gli strumenti terzi da questo utilizzati si avvalgono di cookie: se vuoi saperne di piu' o negare il consenso a tutti o ad alcuni cookie, consulta la ",
+    // Text button to accept cookie(s)
+    "dismiss":"Ok",
+    // Text link of your cookie policy page
+    "learnMore":"Cookie policy",
+    // This is the link to your cookie policy page
+    "link":"index.php?mod=none_Cookie_Policy",
+    // CSS Theme to use
+    // [dark-top | dark-bottom | dark-floating | light-top | light-bottom | light-floating]
+    "theme":"dark-bottom"
+};
