3fd8c607189a930a403e9fa936c417fcac7b174d
Marco Segato <marco.segato@gmail.com>
2015-06-14 14:49:24 +0200

    Cookie policy page

---
+++ section.php
@@ -0,0 +1,32 @@
+<?php
+
+if (preg_match("/section.php/i",$_SERVER['PHP_SELF']))
+{ Header("Location: ../../index.php");
+    die();
+}
+
+?>
+
+<p>In linea con la legislazione Europea, questo sito garantisce che ogni utente del sito web comprenda cosa sono i cookies e per quale motivo vengono utilizzati, in modo che gli utenti stessi possano decidere consapevolmente se accettarne l'utilizzo o no sui loro devices.</p>
+<p>Un cookie &egrave; un piccolo file di testo contenente un numero di identificazione univoco che viene trasferito dal sito web sul disco rigido del vostro computer attraverso un codice anonimo in grado di identificare il computer ma non l'utente e di monitorare passivamente le vostre attivit&agrave; sul sito.</p>
+<h3>Le diverse categorie di cookies</h3>
+<p>I cookies possono essere classificati in quattro categorie, in base alle loro funzionalit&agrave;:</p>
+<ul>
+<li>Cookies strettamente necessari</li>
+<li>Cookies sulle prestazioni</li>
+<li>Cookies funzionali</li>
+<li>Targeting cookies e cookies di pubblicit&agrave;</li>
+</ul>
+<ol>
+<li>Cookies strettamente necessari: Questi cookies sono essenziali al fine di permettere la navigazione del sito web e l'utilizzo delle sue funzionalit&agrave;, come ad esempio l'accesso ad alcune aree protette. Senza questi cookies, alcune funzionalit&agrave; richieste come ad esempio il login al sito o la creazione di un carrello per lo shopping online non potrebbero essere fornite.</li>
+<li>Cookies sulle prestazioni: Questi cookies raccolgono informazioni su come gli utenti utilizzano il sito web, ad esempio quali pagine vengono visitate pi&ugrave; spesso, e se gli utenti ricevono messaggi di errore da queste pagine. Questi cookies non raccolgono informazioni che identificano un visitatore specifico. Tutte le informazioni raccolte da questi cookies sono aggregate e quindi anonime. Vengono utilizzate unicamente per migliorare il funzionamento del sito web.</li>
+<li>Cookie funzionali: Questi cookie consentono al sito Web di ricordare le scelte che avete effettuato ( come il vostro username, la vostra lingua o l'area geografica in cui vivete ) al fine di ottimizzare e fornire funzionalit&agrave; pi&ugrave; avanzate. Questi cookies possono inoltre essere utilizzati per fornire funzionalit&agrave; da voi richieste come ad esempio la visualizzazione di un video o la possibilit&agrave; di commentare un blog. Queste informazioni raccolte dai cookies possono essere anonime e non devono tracciare la navigazione e le attivit&agrave; dell'utente su altri siti web.</li>
+<li>Targeting cookies e cookies di pubblicit&agrave;: Sono i cookie utilizzati per tracciare la navigazione dell'utente in rete e creare profili sui suoi gusti, abitudini, scelte, ecc. Con questi cookie possono essere trasmessi al terminale dell'utente messaggi pubblicitari in linea con le preferenze gi&agrave; manifestate dallo stesso utente nella navigazione online.</li>
+</ol>
+<p>Vi preghiamo di considerare che alcune funzionalit&agrave; del sito web potrebbero essere ridotte o non disponibili disabilitando i cookies.</p>
+<h3>Cookie di questo sito</h3>
+<p>Questo sito utilizza i cookie strettamente necessari e i cookie funzionali</p>
+<h3>Come posso controllare l'installazione di Cookie?</h3>
+<p>In aggiunta a quanto indicato in questo documento, l'Utente pu&ograve; gestire le preferenze relative ai Cookie direttamente all'interno del proprio browser ed impedire &ndash; ad esempio &ndash; che terze parti possano installarne. Tramite le preferenze del browser &egrave; inoltre possibile eliminare i Cookie installati in passato, incluso il Cookie in cui venga eventualmente salvato il consenso all'installazione di Cookie da parte di questo sito. &Egrave; importante notare che disabilitando tutti i Cookie, il funzionamento di questo sito potrebbe essere compromesso. L'Utente pu&ograve; trovare informazioni su come gestire i Cookie nel suo browser ai seguenti indirizzi: <a href="https://support.google.com/chrome/answer/95647?hl=it&amp;p=cpn_cookies" target="_blank">Google Chrome</a>, <a href="https://support.mozilla.org/it/kb/Attivare%20e%20disattivare%20i%20cookie" target="_blank">Mozilla Firefox</a>, <a href="https://support.apple.com/kb/PH19214?viewlocale=it_IT&amp;locale=en_US" target="_blank">Apple Safari</a> and <a href="http://windows.microsoft.com/it-it/windows-vista/block-or-allow-cookies" target="_blank">Microsoft Windows Explorer</a>.</p>
+<p>In caso di servizi erogati da terze parti, l'Utente pu&ograve; inoltre esercitare il proprio diritto ad opporsi al tracciamento informandosi tramite la privacy policy della terza parte, tramite il link di opt out se esplicitamente fornito o contattando direttamente la stessa.</p>
+<p>Fermo restando quanto precede, il Titolare informa che l'Utente pu&ograve; avvalersi di <a href="http://www.youronlinechoices.com/" target="_blank">Your Online Choices</a>. Attraverso tale servizio &egrave; possibile gestire le preferenze di tracciamento della maggior parte degli strumenti pubblicitari. Il Titolare, pertanto, consiglia agli Utenti di utilizzare tale risorsa in aggiunta alle informazioni fornite dal presente documento.</p>
